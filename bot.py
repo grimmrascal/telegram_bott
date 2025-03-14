a8b899ad9273c2451b8a23cfe6b3030f3b8d5f08
grimmrascal <65977736+grimmrascal@users.noreply.github.com>
2025-03-14 17:36:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,3 @@
-import os
-from aiogram import Bot
-from aiogram.enums import ParseMode
-
-# Зчитуємо токен з змінної середовища
-TOKEN = os.getenv("TOKEN")
-
-if not TOKEN:
-    raise ValueError("❌ Токен не знайдено! Перевірте файл .env.")
-
-bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
 from dotenv import load_dotenv
 import os
 
