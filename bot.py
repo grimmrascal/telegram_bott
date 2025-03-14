1dabc754ad7d4475b1ee10340f0ae212e7798324
grimmrascal <65977736+grimmrascal@users.noreply.github.com>
2025-03-14 17:35:58 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,3 +9,13 @@ if not TOKEN:
     raise ValueError("❌ Токен не знайдено! Перевірте файл .env.")
 
 bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+from dotenv import load_dotenv
+import os
+
+# Завантажуємо змінні середовища з файлу .env
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+if not TOKEN:
+    raise ValueError("❌ Токен не знайдено! Перевірте файл .env.")
